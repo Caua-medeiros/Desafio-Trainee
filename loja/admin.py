2e9ef1be9e056e8917b48fee9c76eb387856abc1
Cauã-Medeiros <cauamedeiros@ejectufrn.com.br>
2026-05-26 20:00:35 -0300

    Criação do cadastro(clientes, logistas, produtos) e login

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Cadastro_Clientes, Cadastro_Logistas, Login, Cadastro_Produtos
 
-# Register your models here.
+class CadastroClientesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_completo', 'data_nascimento', 'telefone', 'cpf', 'cep', 'email')
+    list_display_links = ('id', 'nome_completo', 'email')
+    list_per_page = 20
+    search_fields = ('nome_completo', 'email')
+
+admin.site.register(Cadastro_Clientes, CadastroClientesAdmin)
+
+class CadastroLogistasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_completo', 'data_nascimento', 'telefone', 'cpf', 'cep', 'email')
+    list_display_links = ('id', 'nome_completo', 'email')
+    list_per_page = 20
+    search_fields = ('nome_completo', 'email')
+
+admin.site.register(Cadastro_Logistas, CadastroLogistasAdmin)
+
+class LoginAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'senha')
+    list_display_links = ('id', 'email')
+    list_per_page = 20
+    search_fields = ('email',)
+
+admin.site.register(Login, LoginAdmin)
+
+class CadastroProdutosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_produto', 'descricao', 'preco', 'tamanho', 'estoque')
+    list_display_links = ('id', 'nome_produto')
+    list_per_page = 20
+    search_fields = ('nome_produto',)
+
+admin.site.register(Cadastro_Produtos, CadastroProdutosAdmin)
+
--- loja/models.py
+++ loja/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-
-'''Id
-Nome_Completo
-Data_Nascimento
-Telefone
-CPF
-CEP
-E-mail
-Senha'''
-
-class Cadrastro(models.Model):
+class Cadastro_Clientes(models.Model):
     nome_completo = models.CharField(blank=False, max_length=100)
     data_nascimento = models.DateField(blank=False)
     telefone = models.CharField(blank=False, max_length=20)
@@ -21,4 +11,38 @@ class Cadrastro(models.Model):
     def __str__(self):
         return self.nome_completo
     
-class 
+class Cadastro_Logistas(models.Model):
+    nome_completo = models.CharField(blank=False, max_length=100)
+    data_nascimento = models.DateField(blank=False)
+    telefone = models.CharField(blank=False, max_length=20)
+    cpf = models.CharField(blank=False, max_length=11)
+    cep = models.CharField(blank=False, max_length=9)
+    email = models.EmailField(blank=False, max_length=100)
+    senha = models.CharField(blank=False, max_length=100)
+
+    def __str__(self):
+        return self.nome_completo
+    
+class Login(models.Model):
+    email = models.EmailField(blank=False, max_length=100)
+    senha = models.CharField(blank=False, max_length=100)
+
+    def __str__(self):
+        return self.email
+    
+
+class Cadastro_Produtos(models.Model):
+    nome_produto = models.CharField(blank=False, max_length=100)
+    descricao = models.TextField(blank=False)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    TAMANHO = (
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+        ('GG', 'Extra Grande'),
+    )
+    tamanho = models.CharField(max_length=2, choices=TAMANHO, blank=False, null=True, default='M')
+    estoque = models.IntegerField()
+
+    def __str__(self):
+        return self.nome_produto
